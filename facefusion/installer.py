2e99f1a4dd06deacf6d9d66822996bc71b616d39
jonhvnn <146161042+jonhvnn@users.noreply.github.com>
2023-12-01 13:14:09 +0700

    Update installer.py

--- facefusion/installer.py
+++ facefusion/installer.py
@@ -1,9 +1,6 @@
 from typing import Dict, Tuple
-import argparse
-import os
-import sys
 import subprocess
-import tempfile
+from argparse import ArgumentParser, HelpFormatter
 
 subprocess.call([ 'pip', 'install' , 'inquirer', '-q' ])
 
@@ -11,6 +8,13 @@ import inquirer
 
 from facefusion import metadata, wording
 
+TORCH : Dict[str, str] =\
+{
+	'default': 'default',
+	'cpu': 'cpu',
+	'cuda': 'cu118',
+	'rocm': 'rocm5.6'
+}
 ONNXRUNTIMES : Dict[str, Tuple[str, str]] =\
 {
 	'default': ('onnxruntime', '1.16.3'),
@@ -22,47 +26,38 @@ ONNXRUNTIMES : Dict[str, Tuple[str, str]] =\
 }
 
 
-def run() -> None:
-	program = argparse.ArgumentParser(formatter_class = lambda prog: argparse.HelpFormatter(prog, max_help_position = 120))
-	program.add_argument('--onnxruntime', help = wording.get('onnxruntime_help'), dest = 'onnxruntime', choices = ONNXRUNTIMES.keys())
-	program.add_argument('-v', '--version', version = metadata.get('name') + ' ' + metadata.get('version'), action = 'version')
+def cli() -> None:
+    program = ArgumentParser(formatter_class=lambda prog: HelpFormatter(prog, max_help_position=120))
+    program.add_argument('--torch', help=wording.get('install_dependency_help').format(dependency='torch'), dest='torch', choices=TORCH.keys(), default='cuda')  # Set default value to 'cuda'
+    program.add_argument('--onnxruntime', help=wording.get('install_dependency_help').format(dependency='onnxruntime'), dest='onnxruntime', choices=ONNXRUNTIMES.keys(), default='cuda')  # Set default value to 'cuda'
+    program.add_argument('-v', '--version', version=metadata.get('name') + ' ' + metadata.get('version'), action='version')
+    run(program)
+
+
+def run(program : ArgumentParser) -> None:
 	args = program.parse_args()
 
-	if args.onnxruntime:
+	if args.torch and args.onnxruntime:
 		answers =\
 		{
+			'torch': args.torch,
 			'onnxruntime': args.onnxruntime
 		}
 	else:
 		answers = inquirer.prompt(
 		[
-			inquirer.List(
-				'onnxruntime',
-				message = wording.get('onnxruntime_help'),
-				choices = list(ONNXRUNTIMES.keys())
-			)
+			inquirer.List('torch', message = wording.get('install_dependency_help').format(dependency = 'torch'), choices = list(TORCH.keys())),
+			inquirer.List('onnxruntime', message = wording.get('install_dependency_help').format(dependency = 'onnxruntime'), choices = list(ONNXRUNTIMES.keys()))
 		])
-
-	if answers is not None:
+	if answers:
+		torch = answers['torch']
+		torch_wheel = TORCH[torch]
 		onnxruntime = answers['onnxruntime']
 		onnxruntime_name, onnxruntime_version = ONNXRUNTIMES[onnxruntime]
-		python_id = 'cp' + str(sys.version_info.major) + str(sys.version_info.minor)
 		subprocess.call([ 'pip', 'uninstall', 'torch', '-y' ])
-		if onnxruntime == 'cuda':
-			subprocess.call([ 'pip', 'install', '-r', 'requirements.txt', '--extra-index-url', 'https://download.pytorch.org/whl/cu118' ])
-		else:
+		if torch_wheel == 'default':
 			subprocess.call([ 'pip', 'install', '-r', 'requirements.txt' ])
-		if onnxruntime != 'cpu':
-			subprocess.call([ 'pip', 'uninstall', 'onnxruntime', onnxruntime_name, '-y' ])
-		if onnxruntime != 'coreml-silicon':
-			subprocess.call([ 'pip', 'install', onnxruntime_name + '==' + onnxruntime_version ])
-		elif python_id in [ 'cp39', 'cp310', 'cp311' ]:
-			wheel_name = '-'.join([ 'onnxruntime_silicon', onnxruntime_version, python_id, python_id, 'macosx_12_0_arm64.whl' ])
-			wheel_path = os.path.join(tempfile.gettempdir(), wheel_name)
-			wheel_url = 'https://github.com/cansik/onnxruntime-silicon/releases/download/v' + onnxruntime_version + '/' + wheel_name
-			subprocess.call([ 'curl', wheel_url, '-o', wheel_path, '-L' ])
-			subprocess.call([ 'pip', 'install', wheel_path ])
-			os.remove(wheel_path)
-            
-
-            
+		else:
+			subprocess.call([ 'pip', 'install', '-r', 'requirements.txt', '--extra-index-url', 'https://download.pytorch.org/whl/' + torch_wheel ])
+		subprocess.call([ 'pip', 'uninstall', 'onnxruntime', onnxruntime_name, '-y' ])
+		subprocess.call([ 'pip', 'install', onnxruntime_name + '==' + onnxruntime_version ])
